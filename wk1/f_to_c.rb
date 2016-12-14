8901219753b030a7e58c486d47da3d64c9d51583
Anthony Guidarelli <anthony.guidarelli@gmail.com>
2016-12-13 19:29:29 -0500

    First commit.

---
+++ wk1/f_to_c.rb
@@ -0,0 +1,8 @@
+def convert_f(f_degrees)
+  (f_degrees.to_i - 32) * (5.0/9.0)
+end
+
+puts "Enter a temperature in Fahrenheit:"
+f = gets.chomp
+celcius = convert_f(f)
+puts "That is #{celcius.round(1)} degrees Celcius"
